07482c2bdc04cce0d40c6736631f33dd65d7fa65
shivamsoin <68854577+shivamsoin@users.noreply.github.com>
2020-12-29 15:07:27 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,85 @@
+import random
+
+rock = """
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+"""
+
+paper = """
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+"""
+
+scissors = """
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+"""
+
+user_val = input('Enter 0 for Rock 1 for Paper and 2 for Scissors')
+comp_val = random.randint(0, 2)
+if int(user_val) == 0 and comp_val == 0:
+    print('You chose')
+    print(rock)
+    print('Computer chose')
+    print(rock)
+    print('Tie')
+elif int(user_val) == 0 and comp_val == 1:
+    print('You chose')
+    print(rock)
+    print('Computer chose')
+    print(paper)
+    print('Computer Wins!!')
+elif int(user_val) == 0 and comp_val == 2:
+    print('You chose')
+    print(rock)
+    print('Computer chose')
+    print(scissors)
+    print('You Win!!')
+elif int(user_val) == 1 and comp_val == 1:
+    print('You chose')
+    print(paper)
+    print('Computer chose')
+    print(paper)
+    print('Tie')
+elif int(user_val) == 1 and comp_val == 0:
+    print('You chose')
+    print(paper)
+    print('Computer chose')
+    print(rock)
+    print('You Win!!')
+elif int(user_val) == 1 and comp_val == 2:
+    print('You chose')
+    print(paper)
+    print('Computer chose')
+    print(scissors)
+    print('Computer Wins!!')
+elif int(user_val) == 2 and comp_val == 2:
+    print('You chose')
+    print(scissors)
+    print('Computer chose')
+    print(scissors)
+    print('Tie')
+elif int(user_val) == 2 and comp_val == 1:
+    print('You chose')
+    print(scissors)
+    print('Computer chose')
+    print(paper)
+    print('You Win!!')
+elif int(user_val) == 2 and comp_val == 0:
+    print('You chose')
+    print(scissors)
+    print('Computer chose')
+    print(rock)
+    print('Computer Wins!!')
